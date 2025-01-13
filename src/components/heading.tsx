594ac516e620f400aa52f1d0dbdb197f3a744ffc
thbueno <thiagobueno.s@gmail.com>
2025-01-13 11:06:46 +0700

    Finish discord chat and start building Bento Grid

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -1,14 +1,20 @@
-import { cn } from "@/utils";
-import { HTMLAttributes, ReactNode } from "react";
+import { cn } from "@/utils"
+import { HTMLAttributes, ReactNode } from "react"
 
 interface HeadingProps extends HTMLAttributes<HTMLHeadElement> {
-    children?: ReactNode;
-};
+  children?: ReactNode
+}
 
-export const Heading  = ({children, className, ...props}: HeadingProps) => {
-    return (
-        <h1 className={cn("text-4xl sm:text-5xl text-pretty font-heading font-semibold tracking-tight text-zinc-800", className)} {...props}>
-            {children}
-        </h1>
-    )
+export const Heading = ({ children, className, ...props }: HeadingProps) => {
+  return (
+    <h1
+      className={cn(
+        "text-4xl text-center sm:text-5xl text-pretty font-heading font-semibold tracking-tight text-zinc-800",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </h1>
+  )
 }
